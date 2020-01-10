3fe9a29da26c6c3d444fcacf209b7cf4aacadf02
wadeepat <leaw.wadeepat@gmail.com>
2020-01-10 15:39:20 +0700

    finished

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string name;
+    double gpa;
+
+    cout<<"What is your name?: ";
+    cin>>name;
+
+    cout<<"What is your GPA?: ";
+    cin>>gpa;
+
+    if (gpa>=3.50)
+    {
+        cout<<name<<" InW Jrim Jrim!!!";
+    }else
+    {
+        cout<<"Try harder, "<<name<<"!!!";
+    }
+
+}
 
